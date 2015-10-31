9fc8416a6d6cbaccce4136e648740e47d2144f5c
Alex Aralis <alex@gmail.com>
2015-10-31 16:07:47 -0700

    ive been messing with closing subprocesses running reactor/autobahn... it's a disaster. but ive get it working by calling SIGTERM from the parent proc. this commit is to save the commented out code that doent work in case it is needed for something later.

--- mysqlConsoleServer.py
+++ mysqlConsoleServer.py
@@ -1,7 +1,7 @@
 import time
 from subprocess import Popen
 from twisted.internet.defer import inlineCallbacks
-from twisted.internet import reactor
+from twisted.internet import reactor, task
 from autobahn.twisted.util import sleep
 from autobahn.twisted.wamp import ApplicationSession, ApplicationRunner
 
@@ -9,23 +9,35 @@ def srv_log(msg):
     print('[server] ' + msg)
 
 class Server(ApplicationSession):
-
+    sessions = {}
     @inlineCallbacks
     def onJoin(self, details):
         srv_log('server joined')
-   
+
         def createSession():
             global sessionCount
             srv_log('session requested')
             creation = str(int(time.time()*1000))
-            Popen(['python','mysqlConsoleSession.py', creation])
+            self.sessions[creation] = Popen(['python','mysqlConsoleSession.py', creation])
+
+#            def monitor():
+#                if not (newP.poll() is None) :
+#                    srv_log('waiting for subproc to exit: ' + str(newP.poll()))
+#                    l.stop() 
+#                    newP.wait()
+#                    newP.terminate()
+
+#            l = task.LoopingCall(monitor)
+#            l.start(1)
             srv_log('mysqlConsoleSession.py called: ' + creation)
             return creation
 
-#        def meep(msg):
-#            srv_log(msg)
-#        try:
+        def closeSession(sessionName):
+            self.sessions[sessionName].terminate()
+            return 'session '+sessionName+' closed'
+
         yield self.register(createSession, u'com.mysql.console.requestSession')
+        yield self.register(closeSession, u'com.mysql.console.closeSession')
 #        except Exception  as e:
 #            srv_log(str(e))
 #            srv_log('it didnt work')
--- mysqlConsoleSession.py
+++ mysqlConsoleSession.py
@@ -7,12 +7,12 @@ from autobahn.twisted.util import sleep
 from autobahn.twisted.wamp import ApplicationSession, ApplicationRunner
 from autobahn.wamp.exception import ApplicationError
 def sess_log(msg):
-    print("[session " + sys.argv[1] + "]: " + msg)
+    print("[pid:" + str(os.getpid()) + " session:" + sys.argv[1] + "]: " + str(msg))
 
 class mysqlSession(ApplicationSession):
     @inlineCallbacks
     def onJoin(self, details):
-        sess_log(' mysqlSession has joined')
+        sess_log('mysqlSession has joined')
 
         def repeat(msg):
             sess_log(msg)
@@ -20,36 +20,44 @@ class mysqlSession(ApplicationSession):
 
         def closeSession():
             sess_log('session exiting')
-            reactor.stop()
+            self.l.stop()
+            #reactor.stop()
+            #d.cancel() 
+            #reactor.stop()
+            os.kill(os.getpid(), signal.SIGTERM)
+            #sys.exit(32)
+            sess_log('printed after int')
             
         @inlineCallbacks
         def ping():
             watchdog = reactor.callLater(2, closeSession)
             try:
                 yield self.call(u'com.mysql.console.pong.' + sys.argv[1])
+                watchdog.cancel()
             except ApplicationError:
                 sess_log('pong not found!!!')
+                watchdog.cancel()
                 closeSession()
             
-            watchdog.cancel()
         
-        self.l = task.LoopingCall(ping)
-        self.l.start(2)
-
-        sess_log(' registering function query')
+        sess_log('registering rpc query and close')
         yield self.register(repeat, u'com.mysql.console.query.' + sys.argv[1])
         yield self.register(closeSession , u'com.mysql.console.close.' + sys.argv[1])
-        sess_log(' query registered')
-     
+        sess_log('rpcs registered')
+        
+        self.l = task.LoopingCall(ping)
+        self.l.start(2) 
 
     def onLeave(self, details):
         sess_log('Leaving: ' + str(details)) 
-        self.l.stop()
-        os.kill(os.getpid(), signal.SIGKILL)
             
+    def onDisconnect(self):
+        sess_log('Disconnecting: ');
+        
 if __name__ == '__main__':
-    sess_log(' running as main')
+    sess_log('running as main')
     runner = ApplicationRunner(u'ws://localhost:8081/ws', u'realm1')
-    runner.run(mysqlSession, False)
+    d = runner.run(mysqlSession, False)
+    d.addErrback(sess_log)
     reactor.run()
     sess_log('process ending')
